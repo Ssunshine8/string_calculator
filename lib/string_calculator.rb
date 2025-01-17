ff66a6b8e4ada2bef1d56a6b26d807087a5f8184
Ashlesha <ashleshasingh8@gmail.com>
2025-01-17 19:14:47 +0530

    add custom delimiters and handle negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,25 @@
 class StringCalculator
-  def add(numbers)
-    return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+  def add(numbers)
+    return 0 if numbers.nil? || numbers.empty? 
+
+    delimiter, numbers = get_delimiter_and_numbers(numbers)
+    delimiters_regex = Regexp.union([delimiter, ",", "\n"].compact)
+    number_array = numbers.split(delimiters_regex).map(&:to_i)
+
+    negative_number = number_array.select { |num| num < 0 }
+    raise "negative numbers not allowed: #{negative_number.join(', ')}" unless negative_number.empty?
+
+    number_array.sum
+  end
+
+  private
+
+  def get_delimiter_and_numbers(numbers)
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      return [Regexp.escape(delimiter), numbers]
+    end
+    [nil, numbers]
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,28 +1,41 @@
 require 'string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   it 'returns 0 for an empty string' do
-    calculator = StringCalculator.new
     expect(calculator.add("")).to eq(0)
   end
 
+  it "returns 0 for nil input" do
+    expect(calculator.add(nil)).to eq(0)
+  end
+
   it 'returns the number itself for a single number' do
-    calculator = StringCalculator.new
     expect(calculator.add("1")).to eq(1)
   end
 
   it 'returns the sum of two numbers' do
-    calculator = StringCalculator.new
     expect(calculator.add("1,2")).to eq(3)
   end
 
   it 'returns the sum of multiple numbers' do
-    calculator = StringCalculator.new
     expect(calculator.add("1,2,3,4")).to eq(10)
   end
 
   it 'handles newlines as delimiters' do
-    calculator = StringCalculator.new
     expect(calculator.add("1\n2,3")).to eq(6)
+  end
+
+  it 'handles custom delimiters' do
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
+
+  it 'return an error for negative numbers' do
+    expect { calculator.add("1,-2,3,4") }.to raise_error("negative numbers not allowed: -2")
+  end
+
+  it "return an error for multiple negative numbers" do
+    expect { calculator.add("//;\n1;-2;-3;4") }.to raise_error("negative numbers not allowed: -2, -3")
   end
 end
